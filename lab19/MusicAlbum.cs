06579102c5d89dee583b02dcdb64f4eafab4fdad
mrmarkiz <maxtomusiak315@gmail.com>
2023-06-01 22:06:21 +0300

    add task2

---
+++ lab19/MusicAlbum.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab19
+{
+    [DataContract]
+
+    internal class MusicAlbum
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Maker { get; set; }
+
+        [DataMember]
+        public int PublicationYear { get; set; }
+
+        [DataMember]
+        public double DurationMin { get; set; }
+
+        [DataMember]
+        public string Studio { get; set; }
+
+        public MusicAlbum(string name, string maker, int year, double dur, string studio)
+        {
+            Name = name;
+            Maker = maker;
+            PublicationYear = year;
+            DurationMin = dur;
+            Studio = studio;
+        }
+
+        public MusicAlbum() : this("", "", 0, 0, "")
+        { }
+
+        public void Init()
+        {
+            Console.Write("Enter name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter Maker: ");
+            Maker = Console.ReadLine();
+            Console.Write("Enter Publication year: ");
+            int.TryParse(Console.ReadLine(), out int year);
+            PublicationYear = year;
+            Console.Write("Enter duration(minuutes): ");
+            double.TryParse(Console.ReadLine(), out double dur);
+            DurationMin = dur;
+            Console.Write("Enter studio: ");
+            Studio = Console.ReadLine();
+        }
+
+        public void Save()
+        {
+            string path = "D:\\VisualStudio\\Projects C#\\lab19\\lab19\\text2.dat";
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                DataContractSerializer dcs = new DataContractSerializer(typeof(MusicAlbum));
+                dcs.WriteObject(fs, this);
+            }
+            Console.WriteLine("Object serialized");
+        }
+
+        public static MusicAlbum Read()
+        {
+            MusicAlbum album;
+            string path = "D:\\VisualStudio\\Projects C#\\lab19\\lab19\\text2.dat";
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                DataContractSerializer dcs = new DataContractSerializer(typeof(MusicAlbum));
+                try
+                {
+                    album = (MusicAlbum)dcs.ReadObject(fs);
+                }
+                catch
+                {
+                    album = new MusicAlbum();
+                }
+            }
+            return album;
+        }
+
+        public string ToLongString()
+        {
+            return $"Name: {Name}, Maker: {Maker}, Publiation year: {PublicationYear}, Duration(mins): {DurationMin}, Studio: {Studio}";
+        }
+    }
+}
---
+++ lab19/Task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab19
+{
+    internal class Task2
+    {
+        public static void Run()
+        {
+            MusicAlbum musicAlbum = new MusicAlbum();
+            int choice;
+            do
+            {
+                Console.WriteLine("Enter what to do(1-show, 2-init, 3-save, 4-load):");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine(musicAlbum.ToLongString());
+                        break;
+                    case 2:
+                        musicAlbum.Init();
+                        break;
+                    case 3:
+                        musicAlbum.Save();
+                        break;
+                    case 4:
+                        musicAlbum = MusicAlbum.Read();
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
